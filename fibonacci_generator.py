278c557583e2b8e1c09849a78b96ca1a00f9a8f2
Varsha kadam <166228996+varsha614@users.noreply.github.com>
2024-04-15 18:17:47 +0530

    Update fibonacci_generator.py

--- fibonacci_generator.py
+++ fibonacci_generator.py
@@ -1,14 +1,10 @@
-def fibonacci_generator(n):
-    fibonacci_sequence = [0, 1]  # Initialize the sequence with the first two Fibonacci numbers
-    
-    # Generate Fibonacci sequence up to the nth term
-    for i in range(2, n):
+def fibonacci_generator(N):
+    fibonacci_sequence = [0, 1]
+    for i in range(2, N):
         next_fibonacci = fibonacci_sequence[-1] + fibonacci_sequence[-2]
         fibonacci_sequence.append(next_fibonacci)
-    
     return fibonacci_sequence
-
-n = int(input("Enter a number: "))
-fibonacci_sequence = fibonacci_generator(n)
-print("Fibonacci sequence up to the", n, "th term:")
+N = int(input("Enter a number: "))
+fibonacci_sequence = fibonacci_generator(N)
+print("Fibonacci sequence up to the", N, "th term:")
 print(fibonacci_sequence)
